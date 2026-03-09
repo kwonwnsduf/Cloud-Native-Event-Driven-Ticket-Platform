e379345e6c1ea9c0d164c77b82e6e14f221a4b0a
kwonwnsduf <junyeol1009@gmail.com>
2026-03-09 22:17:58 +0900

    feat: add user-service skeleton

---
+++ services/user-service/src/main/java/com/ticketplatform/user_service/controller/HealthController.java
@@ -0,0 +1,12 @@
+package com.ticketplatform.user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health(){
+        return "OK";
+    }
+}
